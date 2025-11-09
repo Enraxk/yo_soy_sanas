9b6205b35cef64a0df9da8294975b6647776e192
Eneko Lapuente Bascuñana <enekolapuentebas@gmail.com>
2025-11-09 15:39:09 +0100

    File Remove

--- app/api/logout/route.ts
+++
@@ -1,6 +0,0 @@
-﻿import { NextResponse } from "next/server";
-
-export async function POST() {
-  // Aquí podrías limpiar la sesión si usas cookies o tokens
-  return NextResponse.json({ ok: true });
-}
